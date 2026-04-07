80afe1ce667264e6ff78c950143094307cfb9f2b
Rajkumar <thecodeheist01@gmail.com>
2026-04-07 15:29:04 +0530

    basic_schema_added

--- apps/http-server/src/routes/authRoutes.ts
+++ apps/http-server/src/routes/authRoutes.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express";
 import { errorResponse } from "../utils/error";
 import { prisma } from "@repo/db";
 import { JWT_SECRET } from "@repo/common";
+import bcrypt from 'bcrypt';
 
 const router:Router = Router();
 
@@ -15,15 +16,41 @@ router.post("/signup", async(req:Request, res:Response) => {
         }
          
         // logic to find already existed user
-        const isUserExistsAlready = null;
+        const isUserExistsAlready = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
 
-        
+        if(isUserExistsAlready) return errorResponse(res, 400, "Email already exists.Try another email")
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+         const user = await prisma.user.create({
+            data : {
+                username,
+                email,
+                password: hashedPassword
+            }
+         });
+
+         // token login -> put it in common
+         
+         return res.status(201).json({
+            user: {
+                userId: user.id,
+                username: user.username,
+                email: user.email
+            }
+         })
+
 
 
 
 
     } catch (error) {
-        
+        console.log(error);
+        return errorResponse(res, 500, "Internal Server Error..")
     }
 
 
